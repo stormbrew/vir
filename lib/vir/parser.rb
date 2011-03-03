abb7a77c3200f7f21e43c370780f1e77bfef9925
Graham <graham-storm-git@ript.net>
2011-03-02 23:42:24 -0700

    Major improvements to whitespace and comment handling.

--- lib/vir/parser.rb
+++ lib/vir/parser.rb
@@ -5,7 +5,30 @@ module Vir
     rule(:ws) { match('[ \t]').repeat(1) }
     rule(:ws?) { ws.maybe }
     rule(:br) { str("\n") }
-    rule(:all_ws) { (ws | br | comment).repeat }
+
+    rule(:line_comment_text) { (br.absnt? >> any).repeat }
+    rule(:multiline_comment_text) { (str('*/').absnt? >> any).repeat }
+
+    rule(:comment) { 
+      str('#') >> line_comment_text.as(:text) |
+      str('//') >> line_comment_text.as(:text) |
+      str('/*') >> multiline_comment_text.as(:text) >> str('*/')
+    }
+
+    # Empty whitespace is any whitespace that has no content of any sort (
+    # so unlike iws, it doesn't include comments).
+    # Basically, it's whitespace before something really starts.
+    rule(:ews) { (ws | br).repeat(1) }
+    rule(:ews?) { ews.maybe }
+
+    # Inner whitespace is space between elements of the same line.
+    # It can include spaces, line breaks, and comments.
+    rule(:iws) { (ws | br | comment.as(:inline_doc)).repeat(1) }
+    rule(:iws?) { iws.maybe }
+
+    # End of line can be any amount of whitespace, including comments,
+    # and ending in either a \n or semicolon
+    rule(:eol) { (ws | comment.as(:inline_doc)).repeat >> (br | str(';')) }
 
     rule(:symbol) {
       match('[a-zA-Z_]') >> match('[a-zA-Z0-9_]').repeat
@@ -37,10 +60,10 @@ module Vir
     }
 
     rule(:map_const) {
-      (str("{") >> ws? >> (expression.as(:key) >> ws? >> str(':') >> ws? >> expression.as(:value) >> ws? >> str(',').maybe).repeat.as(:entries) >> ws? >> str('}')).as(:map)
+      (str("{") >> iws? >> (expression.as(:key) >> iws? >> str(':') >> iws? >> expression.as(:value) >> iws? >> str(',').maybe).repeat.as(:entries) >> iws? >> str('}')).as(:map)
     }
     rule(:list_const) {
-      (str("[") >> ws? >> (expression >> ws? >> str(",").maybe).repeat.as(:entries) >> ws? >> str("]")).as(:list)
+      (str("[") >> iws? >> (expression >> iws? >> str(",").maybe).repeat.as(:entries) >> iws? >> str("]")).as(:list)
     }
 
     rule(:const) {
@@ -67,98 +90,96 @@ module Vir
       (local_var | module_var | instance_var | mod_self | instance_self)
     }
 
+    rule(:prefix_op_expression) {
+      ((str('!') | str('+') | str('-') | str('~')).as(:op) >> iws? >> unary_expression).as(:prefix_op)
+    }
+
     rule(:unary_expression) {
-      (str('(') >> ws? >> expression >> ws? >> str(')')) |
+      (str('(') >> iws? >> expression >> iws? >> str(')')) |
       prefix_op_expression | const | variable
     }
 
     rule(:args) {
-      (ws? >> expression >> (ws? >> str(',') >> ws? >> expression).repeat).as(:args)
+      (iws? >> expression >> (iws? >> str(',') >> iws? >> expression).repeat).as(:args)
     }
 
     rule(:arglist) {
-      str('(') >> ws? >> args.maybe >> ws? >> str(')')
+      str('(') >> iws? >> args.maybe >> iws? >> str(')')
+    }
+
+    rule(:line_statement) {
+      statement >> eol
+    }
+    rule(:tail_statement) {
+      statement >> iws?
+    }
+    rule(:statement_sequence) {
+      (ews? >> line_statement).repeat >> ews? >> tail_statement |
+      (ews? >> line_statement).repeat(1) |
+      ews? >> tail_statement
     }
 
     rule(:blockbody) {
-      str('{') >> all_ws.maybe >> statement.repeat.as(:lines) >> all_ws.maybe >> str('}')
+      str('{') >> eol.maybe >> statement_sequence.repeat(0,1).as(:lines) >> iws? >> str('}')
     }
 
     rule(:block) {
-      (expression.as(:name) >> ws? >> match(':') >> ws? >> blockbody).as(:block)
+      (expression.as(:name) >> iws? >> match(':') >> iws? >> blockbody).as(:block)
     }
 
     rule(:call_args) {
-      arglist >> ws? >> (block >> ws?).repeat.as(:blocks) |
-      (block >> ws?).repeat(1).as(:blocks)
+      arglist >> iws? >> (block >> iws?).repeat.as(:blocks) |
+      (block >> iws?).repeat(1).as(:blocks)
     }
 
     rule(:call_expression) {
-      (((call_expression | unary_expression).as(:on) >> ws? >> str('.') >> ws? >> symbol.as(:name)).as(:ref) >> call_args.maybe).as(:call) |
-      (symbol.as(:name).as(:ref) >> ws? >> call_args).as(:call) |
-      (unary_expression.as(:on).as(:ref) >> ws? >> call_args).as(:call) |
+      (((call_expression | unary_expression).as(:on) >> iws? >> str('.') >> iws? >> symbol.as(:name)).as(:ref) >> iws? >> call_args.maybe).as(:call) |
+      (symbol.as(:name).as(:ref) >> iws? >> call_args).as(:call) |
+      (unary_expression.as(:on).as(:ref) >> iws? >> call_args).as(:call) |
       unary_expression
     }
 
     rule(:array_index_expression) {
-      (call_expression.as(:from) >> (ws? >> str('[') >> ws? >> expression >> ws? >> str(']')).repeat(1).as(:index)).as(:array_op) |
+      (call_expression.as(:from) >> (iws? >> str('[') >> iws? >> expression >> iws? >> str(']')).repeat(1).as(:index)).as(:array_op) |
       call_expression
     }
 
     rule(:product_op_expression) {
-      (array_index_expression >> (ws? >> (str('*') | str('/') | str('%')).as(:op) >> ws? >> array_index_expression).repeat(1)).as(:product) |
+      (array_index_expression >> (iws? >> (str('*') | str('/') | str('%')).as(:op) >> iws? >> array_index_expression).repeat(1)).as(:product) |
       array_index_expression
     }
 
     rule(:sum_op_expression) {
-      (product_op_expression >> (ws? >> (str('+') | str('-')).as(:op) >> ws? >> product_op_expression).repeat(1)).as(:sum) |
+      (product_op_expression >> (iws? >> (str('+') | str('-')).as(:op) >> iws? >> product_op_expression).repeat(1)).as(:sum) |
       product_op_expression
     }
 
     rule(:bitshift_op_expression) {
-      (sum_op_expression >> (ws? >> (str('>>') | str('<<')).as(:op) >> ws? >> sum_op_expression).repeat(1)).as(:bitshift) |
+      (sum_op_expression >> (iws? >> (str('>>') | str('<<')).as(:op) >> iws? >> sum_op_expression).repeat(1)).as(:bitshift) |
       sum_op_expression
     }
 
     rule(:relational_op_expression) {
-      (bitshift_op_expression >> (ws? >> (str('>') | str('<') | str('<=') | str('>=')).as(:op) >> ws? >> bitshift_op_expression).repeat(1)).as(:relational) |
+      (bitshift_op_expression >> (iws? >> (str('>') | str('<') | str('<=') | str('>=')).as(:op) >> iws? >> bitshift_op_expression).repeat(1)).as(:relational) |
       bitshift_op_expression
     }
 
     rule(:bitwise_op_expression) {
-      (relational_op_expression >> (ws? >> (str('&') | str('^') | str('|')).as(:op) >> ws? >> relational_op_expression).repeat(1)).as(:bitwise) |
+      (relational_op_expression >> (iws? >> (str('&') | str('^') | str('|')).as(:op) >> iws? >> relational_op_expression).repeat(1)).as(:bitwise) |
       relational_op_expression
     }
 
     rule(:logical_op_expression) {
-      (bitwise_op_expression >> (ws? >> (str('&&') | str('||')).as(:op) >> ws? >> bitwise_op_expression).repeat(1)).as(:logical) |
+      (bitwise_op_expression >> (iws? >> (str('&&') | str('||')).as(:op) >> iws? >> bitwise_op_expression).repeat(1)).as(:logical) |
       bitwise_op_expression
     }
 
-    rule(:op_expression) {
-       logical_op_expression
-    }
-
-    rule(:prefix_op_expression) {
-      ((str('!') | str('+') | str('-') | str('~')).as(:op) >> ws? >> unary_expression).as(:prefix_op)
-    }
-
     rule(:expression) {
-      ws? >> (op_expression | unary_expression)
-    }
-
-    rule(:line_comment_text) { (br.absnt? >> any).repeat }
-    rule(:multiline_comment_text) { (str('*/').absnt? >> any).repeat }
-
-    rule(:comment) { 
-      str('#') >> line_comment_text.as(:text) >> br.maybe |
-      str('//') >> line_comment_text.as(:text) >> br.maybe |
-      str('/*') >> multiline_comment_text.as(:text) >> str('*/') >> ws? >> br.maybe
+      logical_op_expression
     }
 
     rule(:statement) {
-      ((ws? >> comment).repeat.as(:doc) >> ws? >> expression).as(:statement) >> (ws? >> (br | str(';'))).repeat |
-      comment.repeat(1).as(:comment)
+      ((ews | comment).repeat.as(:doc) >> ws? >> expression).as(:statement)
     }
 
     # A 'script' in vir always has exactly one statement. The result of this
@@ -166,7 +187,7 @@ module Vir
     # this statement would be a module or class. It can also be followed by any number of
     # unassociated comments.
     rule(:script) {
-      ws? >> statement >> ws? >> comment.repeat
+      statement >> iws?
     }
 
     root(:script)
